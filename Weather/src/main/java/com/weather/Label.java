69036a593b8ac37fc07a5b4115f3cde20371a88b
dfraga <diego.fraga.m@gmail.com>
2012-04-11 22:13:35 +0200

    Signed-off-by: dfraga <diego.fraga.m@gmail.com>

--- Weather/src/main/java/com/weather/Label.java
+++ Weather/src/main/java/com/weather/Label.java
@@ -6,16 +6,17 @@ public class Label {
 	final int x;
 	final int y;
 
-	Label(byte[] label){
+	Label(final byte[] label) {
 		//XXX if (label.length != 2) error
 		type = LabelType.getLabelType(label[0]);
-		
+
 		x = (((byte)0x00  & 0xFF) << 24) + (((byte)0x00  &  0xFF) << 16)  + (((byte)0x00  &  0xFF) << 8) + ((label[0] & 0x3F)<< 0);
 		y = (((byte)0x00  & 0xFF) << 24) + (((byte)0x00  &  0xFF) << 16)  + (((byte)0x00  &  0xFF) << 8) + ((label[1] & 0xFF)<< 0);
 	}
 
+	@Override
 	public String toString() {
 		return type.name() + " , " + x + " , " + y;
 	}
-	
+
 }
